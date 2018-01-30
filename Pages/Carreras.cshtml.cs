0466d2a7f1bcfb95ad84139a8634df7c9cb62fe5
Bladimir <bladimir.ab@gmail.com>
2018-01-29 21:27:39 -0600

    Add Carreras page

---
+++ Pages/Carreras.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesMovie.Pages
+{
+    public class CarrerasModel : PageModel
+    {
+        public string[] Carreras { get; set; }
+
+        public void OnGet()
+        {
+          Carreras = new string[]{ "ISW", "COFI", "ASM", "OTRA" };
+        }
+    }
+}
