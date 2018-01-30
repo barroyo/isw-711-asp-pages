402458e5b717e8b91611926dc390cad767f4c872
Bladimir <bladimir.ab@gmail.com>
2018-01-29 20:18:52 -0600

    initial commit

---
+++ Pages/About.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesMovie.Pages
+{
+    public class AboutModel : PageModel
+    {
+        // public string Message { get; set; }
+
+        // public string Phone { get; set; }
+
+        public void OnGet()
+        {
+            ViewData["Title"] = "About Us Page";
+            ViewData["Message"] = "Your application description page.";
+            ViewData["Phone"] = "88771677";
+
+        }
+    }
+}
